1a7eaf0a49c6c5cd19a0970770bd0d0ba357e836
a2rworld <ar2r.world@gmail.com>
2023-12-05 16:25:37 -0500

    update ListPoints

--- src/components/ListPoints.js
+++ src/components/ListPoints.js
@@ -1,23 +1,19 @@
-import React, {Component} from 'react'
-class ListPoints extends Component{
-  constructor(props){
-    super()
-    this.state={objects:props.objects, points:props.points, removeObj:props.removeObj}
-    this.renderList = [];
-    this.state.points.forEach((p, i) => {
-      this.renderList.push(
-        <li key={i}>
-          {this.state.objects[i]} - {p}
-          <input type="button" value="x" onClick={()=>this.state.removeObj(i)}/>
-        </li>
-      )
-    })
-  }
+import React from 'react'
 
-  render(){
-    return(<>
-      { this.renderList }
-    </>)
-  }
+const ListPoints = (props) => {
+  const renderList = [];
+  
+  props.points.forEach((p, i) => {
+    renderList.push(
+      <li key={i}>
+        {props.objects[i]} - {p}
+        <input type="button" value="x" onClick={() => props.removeObj(i)}/>
+      </li>
+    )
+  })
+  
+  return <>
+    { renderList }
+  </>
 }
 export default ListPoints
